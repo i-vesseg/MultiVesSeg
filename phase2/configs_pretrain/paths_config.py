5fdca28eccf68fde27a5bcf7dd1db898f3d08639
manigalati <32110860+manigalati@users.noreply.github.com>
2024-05-24 16:52:13 +0200

    Update paths_config.py

--- phase2/configs_pretrain/paths_config.py
+++ phase2/configs_pretrain/paths_config.py
@@ -2,7 +2,7 @@
 
 model_paths = {
     "arcface_model": "/home/galati/phase2/pretrained_models/backbone.pth",
-    "stylegan": "/home/galati/A2V_FL/fedbiomed/Experiment_Vessels_final/final_global_model.pt",
+    "stylegan": "/home/galati/phase1/Experiment_Vessels/final_global_model.pt",
     "metadata": "/home/galati/preprocessing/preprocessing_brain_data/info_OASIS.pkl",
 }
 
@@ -10,7 +10,7 @@ model_paths = {
 
 # model_paths = {
 #     "arcface_model": "/home/galati/phase2/pretrained_models/backbone.pth",
-#     "stylegan": "/home/galati/A2V_FL/fedbiomed/Experiment_MMs_new/final_global_model.pt",
+#     "stylegan": "/home/galati/phase1/Experiment_MMs/final_global_model.pt",
 #     "metadata": "/home/galati/preprocessing/info_MMs_B.pkl",
 # }
 
@@ -18,6 +18,6 @@ model_paths = {
 
 # model_paths = {
 #     "arcface_model": "/home/galati/phase2/pretrained_models/backbone.pth",
-#     "stylegan": "/home/galati/A2V_FL/fedbiomed/Experiment_Modalities_final/final_global_model.pt",
+#     "stylegan": "/home/galati/phase1/Experiment_Modalities/final_global_model.pt",
 #     "metadata": "/home/galati/preprocessing/info_SynthStrip_PDw.pkl",
 # }
